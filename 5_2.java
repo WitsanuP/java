b639c485aa87f92a7ba85720da30fd7f812f7ec7
Balor <122012816+WitsanuP@users.noreply.github.com>
2024-08-05 17:56:36 +0700

    Create 5_2.java

---
+++ 5_2.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+            double amt1 = 2312.126, amt2 = -1425.672;
+            System.out.printf("..... * ....1... *....2.... * ....3....* .... 4%n");
+            System.out.printf(": %12.4f %n" ,amt1 );
+            System.out.printf(": %-12.4f %n", amt2 );
+    }
+
+}
